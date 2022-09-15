2b683e7f18fc1162fc79cf887b7020dcc5940f51
rickyjassoa <shiftmxinfo@gmail.com>
2022-09-15 21:10:11 +0900

    created mobile navbar with transition

--- src/PerrosApp.jsx
+++ src/PerrosApp.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { Catalogo, Contacto, Footer, Header, Nosotros } from './containers';
 import { Navbar } from './components';
 
+import './perrosApp.css'
+
 export const PerrosApp = () => {
   return (
     <div className='App'>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,43 @@
+import { useRef } from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
 
 import './navbar.css';
 
+const Menu = () => (
+  <>
+      <a href="#home">Home</a>
+      <a href="#nosotros">Nosotros</a>
+      <a href="#catalogo">Catalogo</a>
+      <a href="#contacto">Contacto</a>
+  </>
+)
+
+
 const Navbar = () => {
+
+  const navRef = useRef();
+  const btnOpenRef = useRef();
+  const btnCloseRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+    btnOpenRef.current.classList.toggle("hidden");
+  }
+
   return (
-    <div>Navbar</div>
+    <div className='perros__navbar'>
+      <h3>Logo</h3>
+      <div className="perros__navbar-links">
+        <nav ref={navRef}>
+          <Menu/>
+          <button ref={btnCloseRef} className='nav-btn nav-close-btn' onClick={showNavbar}>
+          <FaTimes/>
+          </button>
+        </nav>
+        <button ref={btnOpenRef} className='nav-btn' onClick={showNavbar}>
+          <FaBars/>
+        </button>
+      </div>
+    </div>
   )
 }
 
