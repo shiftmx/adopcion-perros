be1017638e4a9f0c4e5a0faecdfbd820ad0fcd5b
rickyjassoa <shiftmxinfo@gmail.com>
2022-09-16 14:10:48 +0900

    header ready 0.1

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css';
 
+import headerLogo from '../../assets/perros/header-main.png'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='perros__header section__padding'>
+      <div className="perros__header_container">
+        <h1>Donde la adopción de perros es un placer</h1> 
+        <img className='header-logo' src={headerLogo} alt="header logo" />
+        <button className='btn-catalogo'>catalogo</button>
+      </div>
+    </div>
   )
 }
 
