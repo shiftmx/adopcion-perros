15285cd825dc90624b3ddfac145dddb40c38dd6d
rickyjassoa <shiftmxinfo@gmail.com>
2022-09-16 14:33:21 +0900

    header pre slideshow

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,7 +5,7 @@ import headerLogo from '../../assets/perros/header-main.png'
 
 const Header = () => {
   return (
-    <div className='perros__header section__padding'>
+    <div className='perros__header section__padding' id='home'>
       <div className="perros__header_container">
         <h1>Donde la adopción de perros es un placer</h1> 
         <img className='header-logo' src={headerLogo} alt="header logo" />
--- src/containers/nosotros/Nosotros.jsx
+++ src/containers/nosotros/Nosotros.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './nosotros.css';
+import nosotrosImg1 from '../../assets/perros/nosotros_slideshow-1.png'
 
 const Nosotros = () => {
   return (
-    <div>Nosotros</div>
+    <div className='perros__nosotros section__padding' id='nosotros'>
+      <div className="perros__nosotros_container">
+        <h1>¿Quiénes Somos?</h1>
+        <p>
+          Desde 2020, hemos estado trabajando por un futuro mejor para los perros y las personas que los aman. 
+          Cuando un perro está en apuros, lo cuidamos. 
+          Cuando un perro necesita un hogar, le encontramos una familia amorosa. 
+        </p>
+        <p>
+          Para cada perro, para cada dueño, estamos a su lado en las buenas y en las malas, durante toda su vida.
+        </p>
+        <p>
+          Cambia vidas, haciendo cada día más feliz y completo. Es por eso que creemos que un perro es para toda la vida.
+        </p>
+        <img src={nosotrosImg1} alt="Slideshow 1" />
+      </div>
+    </div>
   )
 }
 
