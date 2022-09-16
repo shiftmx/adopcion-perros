82283bb9242af1bbdbb52d1ab220ac201965aaf8
rickyjassoa <shiftmxinfo@gmail.com>
2022-09-16 13:31:59 +0900

    navbar ready 0.1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,24 +18,36 @@ const Navbar = () => {
   const navRef = useRef();
   const btnOpenRef = useRef();
   const btnCloseRef = useRef();
+
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive_nav");
     btnOpenRef.current.classList.toggle("hidden");
+    btnOpenRef.current.classList.remove("visible");
+
   }
+  const hideNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+    btnOpenRef.current.classList.toggle("visible");
+    btnOpenRef.current.classList.remove("hidden");
+  }
+
+
 
   return (
     <div className='perros__navbar'>
+      <div className="perros__navbar_container">
       <h3>Logo</h3>
-      <div className="perros__navbar-links">
-        <nav ref={navRef}>
-          <Menu/>
-          <button ref={btnCloseRef} className='nav-btn nav-close-btn' onClick={showNavbar}>
-          <FaTimes/>
+        <div className="perros__navbar_container-links">
+          <nav ref={navRef}>
+            <Menu/>
+            <button ref={btnCloseRef} className='nav-btn nav-close-btn' onClick={hideNavbar}>
+            <FaTimes/>
+            </button>
+          </nav>
+          <button ref={btnOpenRef} className='nav-btn' onClick={showNavbar}>
+            <FaBars/>
           </button>
-        </nav>
-        <button ref={btnOpenRef} className='nav-btn' onClick={showNavbar}>
-          <FaBars/>
-        </button>
+        </div>
       </div>
     </div>
   )
