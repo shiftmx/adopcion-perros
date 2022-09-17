02cdf4c7b84413ed3521dec2696a02de710df72f
rickyjassoa <shiftmxinfo@gmail.com>
2022-09-17 11:42:47 +0900

    320px ready (pre-slideshow)

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="perros__header_container">
         <h1>Donde la adopción de perros es un placer</h1> 
         <img className='header-logo' src={headerLogo} alt="header logo" />
-        <button className='btn-catalogo'>catalogo</button>
+        <a className='btn-catalogo' href='#catalogo'>catalogo</a>
       </div>
     </div>
   )
